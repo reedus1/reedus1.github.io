0b824fa49ae249438633f5baa2c3924534ea68a1
reedus_ <reedvance1@hotmail.com>
2024-11-14 22:32:51 -0500

    Attempting to Add JS to GitPage
    Adding a simple class that loads JSON data to the resume Webpage.

---
+++ docs/js/load.js
@@ -0,0 +1,28 @@
+function load_data(){
+    fetch('data.json')
+    .then(res => { 
+        if(!res.ok)
+        {
+            throw new Error('Failed to load JSON');
+        }
+        return res.json();
+    }).then(data =>{
+        // Populate the skills and education from JSON data
+        const skillsList = document.getElementById('skills');
+        
+        data.skills.forEach(skill => {
+            const li = document.createElement('li');
+            li.textContent = skill;
+            skillsList.appendChild(li);
+        });
+        //Create Education List 
+        const educationList = document.getElementById('education');
+        data.education.forEach(edu => {
+            const li = document.createElement('li');
+            li.textContent = `${edu.degree} from ${edu.institution} (${edu.year})`;
+            educationList.appendChild(li);
+        });
+    })
+}
+//Load the Json Elements on the Page
+window.onload(load_data());
